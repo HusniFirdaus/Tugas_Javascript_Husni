aa4662c82fd58f8a36d1cb3df77f905b4e67de63
HusniFirdaus <161552922+HusniFirdaus@users.noreply.github.com>
2024-04-24 15:08:09 +0700

    Add files via upload

---
+++ Tugas Javacript 1/qunil.js
@@ -0,0 +1,27 @@
+// latihan soal 1
+var nama = "husni"
+var usia = 20
+var kelas = "PTI"
+var nilai = 70
+console.log(`
+nama : ${nama}\n
+usia : ${usia}\n
+kelas: ${kelas}\n
+nilai: ${nilai}
+`)
+
+// latihan soal 2
+const angka1 = 80
+const angka2 = 78
+console.log(`
+penjumlahan : ${angka1 + angka2}\n
+pengurangan : ${angka1 - angka2}\n
+perkalian : ${angka1 * angka2}\n
+pembagian : ${angka1 / angka2}\n
+`)
+
+// latihan soal 3
+var diskon = 0.2
+var harga = 50
+var totalHarga = harga - (harga * diskon)
+console.log(`Total harga adalah: Rp ${totalHarga}`)
